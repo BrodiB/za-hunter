f20c83b05a8b95b76ca3464246acfed174c54125
Brodi <bbloss@bsd220.org>
2020-03-05 10:20:08 -0600

    Added segue to LocationDetailsViewController

---
+++ za hunter/LocationDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationDetailViewController.swift
+//  za hunter
+//
+//  Created by Brodi Bloss on 3/5/20.
+//  Copyright © 2020 Brodi Bloss. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
